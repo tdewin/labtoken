7bb3182136c336f89a77d3ea41c43afbb7c052bf
tdewin <tdewin@users.noreply.github.com>
2021-11-09 12:17:00 +0100

    adding a very minimal support for protection

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Show the service account token in a web gui for lab envs
 This is not something you want to run in any environment. You are basically showing the password in a GUI :)
 
 Latest dockerhub : tdewin/labtoken:latest
+
+Sample K8s config for K10 (injects k10-k10 SA, works in kasten-io namespace) : https://raw.githubusercontent.com/tdewin/labtoken/main/deploymeinlabonly.yaml
+
+This assumes traefik as ingress, modify as you please
+
+![Screenshot](./media/screen.png)
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 type HTTPHandler struct {
@@ -21,50 +22,95 @@ func main() {
 	log.Println("v 1.0")
 	log.Println("If you run this in production, you are opening the gates")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		token := "couldn't read token, make sure to inject a service account"
-		tokenpath := "/var/run/secrets/kubernetes.io/serviceaccount/token"
+	weakprotection := os.Getenv("WEAKPROTECTION")
+	if weakprotection == "" {
+		weakprotection = "unsecure"
+	}
 
-		content, err := ioutil.ReadFile(tokenpath)
-		if err == nil {
-			token = string(content)
-			log.Println("Serving token")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Query().Get("WEAKPROTECTION") != weakprotection {
+			fmt.Print(`<!doctype html>
+			<html lang="en">
+				<head>
+				<!-- Required meta tags -->
+				<meta charset="utf-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1">
+			
+				<!-- Bootstrap CSS -->
+				<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+			
+				<title>Get your unsecure token here</title>
+				</head>
+				<body onLoad="document.getElementById('token').select()">
+				<div class="container">
+				<div class="row">
+				<div class="col">
+				<h1>Don't run me in ANY Production Environment</h1>
+				</div>
+				</div>
+				<div class="row">
+				  <div class="col">
+				  	<form method="get">
+					  <div class="mb-3">
+						  <label for="weaktoken" class="form-label">Weak Password:</label>
+  						  <input type="email" class="form-control" id="WEAKPROTECTION" placeholder="password">
+					  </div>
+					  <button type="submit" class="btn btn-primary">Submit</button>
+					</form>
+				  </div>
+				</div>
+				  </div>
+			
+				</body>
+			</html>
+			
+			`)
 		} else {
-			log.Printf("Couldn't read token on path %s", tokenpath)
-			log.Println(err)
+			token := "couldn't read token, make sure to inject a service account"
+			tokenpath := "/var/run/secrets/kubernetes.io/serviceaccount/token"
+
+			content, err := ioutil.ReadFile(tokenpath)
+			if err == nil {
+				token = string(content)
+				log.Println("Serving token")
+			} else {
+				log.Printf("Couldn't read token on path %s", tokenpath)
+				log.Println(err)
+			}
+
+			fmt.Fprintf(w, `<!doctype html>
+	<html lang="en">
+		<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+		<!-- Bootstrap CSS -->
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	
+		<title>Get your unsecure token here</title>
+		</head>
+		<body onLoad="document.getElementById('token').select()">
+		<div class="container">
+		<div class="row">
+		<div class="col">
+		<h1>Don't run me in ANY Production Environment</h1>
+		</div>
+		</div>
+		<div class="row">
+		  <div class="col">
+			  <div class="mb-3">
+				  <label for="token" class="form-label">Get your unsecure token here</label>
+				  <textarea onClick="this.select();" class="form-control" id="token" rows="10">%s</textarea>
+			</div>
+		  </div>
+		</div>
+		  </div>
+	
+		</body>
+	</html>`, token)
 		}
 
-		fmt.Fprintf(w, `<!doctype html>
-<html lang="en">
-	<head>
-	<!-- Required meta tags -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<!-- Bootstrap CSS -->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-	<title>Get your unsecure token here</title>
-	</head>
-	<body onLoad="document.getElementById('token').select()">
-	<div class="container">
-	<div class="row">
-	<div class="col">
-	<h1>Don't run me in ANY Production Environment</h1>
-	</div>
-	</div>
-	<div class="row">
-	  <div class="col">
-	  	<div class="mb-3">
-	  		<label for="token" class="form-label">Get your unsecure token here</label>
-	  		<textarea onClick="this.select();" class="form-control" id="token" rows="10">%s</textarea>
-		</div>
-	  </div>
-	</div>
-  	</div>
-
-	</body>
-</html>`, token)
 	})
 	log.Println("Starting  on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
